9f1cba8fb0d174e7d3002d75a908b54ff3519046
norbert lazar <norbert.lazar@flowacademy.hu>
2024-02-11 17:52:03 +0100

    feature (UserExistException) - the response come into JSON with statusCode and message keys

--- projects/norbert-lazar/ads/src/main/java/hu/flowacademy/ads/exceptionHandler/GlobalExceptionHandler.java
+++ projects/norbert-lazar/ads/src/main/java/hu/flowacademy/ads/exceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package hu.flowacademy.ads.exceptionHandler;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,8 +8,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @Getter
+    public static class ErrorResponse {
+        private int statusCode;
+        private String message;
+
+        public ErrorResponse(int statusCode, String message) {
+            this.statusCode = statusCode;
+            this.message = message;
+        }
+
+    }
+
     @ExceptionHandler(UserExistException.class)
-    public ResponseEntity<String> handleUserExistException(UserExistException ex) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleUserExistException(UserExistException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
     }
 }
