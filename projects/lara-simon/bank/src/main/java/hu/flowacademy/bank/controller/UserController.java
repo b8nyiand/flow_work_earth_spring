4698534cb2badc9a611d94168500a72c4f2941e5
ssimonlara <lara.simon@flowacademy.hu>
2024-02-16 00:19:40 +0100

    feat: add controller package

---
+++ projects/lara-simon/bank/src/main/java/hu/flowacademy/bank/controller/UserController.java
@@ -0,0 +1,43 @@
+package hu.flowacademy.bank.controller;
+
+import hu.flowacademy.bank.model.User;
+import hu.flowacademy.bank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.OK)
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/delete/{userName}")
+    public void deleteUser(@PathVariable String userName) {
+        userService.deleteUser(userName);
+    }
+
+    @GetMapping("/{userName}")
+    public User getUserByUserName(@PathVariable String userName) {
+        return userService.getUserByUserName(userName);
+    }
+
+    @GetMapping("/list")
+    public List<User> listAllUsers() {
+        return userService.listAllUsers();
+    }
+}
