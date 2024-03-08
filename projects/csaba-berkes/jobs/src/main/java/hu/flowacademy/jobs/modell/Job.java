c520698eba95013c26b4c365094db29b7deea527
berkescsaba <csaba.berkes@flowacademy.hu>
2024-03-08 15:44:42 +0100

    boilerplate code remove

--- projects/csaba-berkes/jobs/src/main/java/hu/flowacademy/jobs/modell/Job.java
+++ projects/csaba-berkes/jobs/src/main/java/hu/flowacademy/jobs/modell/Job.java
@@ -2,12 +2,17 @@ package hu.flowacademy.jobs.modell;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,9 +27,6 @@ public class Job {
     private Users users;
     private LocalDate creationDate;
 
-    public Job() {
-    }
-
     public Job(String title, int salary, String description, Users users, LocalDate creationDate) {
         this.title = title;
         this.salary = salary;
@@ -32,63 +34,5 @@ public class Job {
         this.users = users;
         this.creationDate = creationDate;
     }
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Users getUsers() {
-        return users;
-    }
-
-    public void setUsers(Users users) {
-        this.users = users;
-    }
-
-    public LocalDate getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDate creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Job{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", salary=" + salary +
-                ", description='" + description + '\'' +
-                ", users=" + users +
-                ", creationDate=" + creationDate +
-                '}';
-    }
 }
